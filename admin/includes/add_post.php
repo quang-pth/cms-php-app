2901544ff26873f41520356b1045292820e6b2b0
QuangPham <quang20201@gmail.com>
2021-11-28 15:02:54 +0700

    link to individual post

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -2,7 +2,7 @@
     if(isset($_POST['create_post'])) {
         $post_title = preg_replace('/[^A-Za-z0-9 \-]/', '', trim($_POST['title']));
         $post_author = preg_replace('/[^A-Za-z0-9 \-]/', '', trim($_POST['author']));
-        $post_category_id = preg_replace('/[^A-Za-z0-9 \-]/', '', trim($_POST['post_category_id']));
+        $post_category_id = preg_replace('/[^A-Za-z0-9 \-]/', '', trim($_POST['post_category']));
         $post_status = preg_replace('/[^A-Za-z0-9 \-]/', '', trim($_POST['post_status']));
         
         $post_image = $_FILES['image']['name'];
@@ -30,8 +30,19 @@
         <input type="text" class="form-control" name="title">
     </div>
     <div class="form-group">
-        <label for="post_category">Post Category Id</label>
-        <input type="text" class="form-control" name="post_category_id">
+        <select name="post_category" id="post_category">
+            <?php 
+                $query = "SELECT * FROM categories";
+                $select_categories = mysqli_query($connection, $query);
+                confirmQuery($select_categories);
+
+                while($row = mysqli_fetch_assoc($select_categories)) {
+                    $cat_id = $row['cat_id'];
+                    $cat_title = $row['cat_title'];
+                    echo "<option value='{$cat_id}'>{$cat_title}</option>";
+                }
+            ?>
+        </select>
     </div>
     <div class="form-group">
         <label for="post_author">Post Author</label>
