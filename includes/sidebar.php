067e6e4371d4c9abe930b03e00424143f72a53f6
QuangPham <quang20201@gmail.com>
2021-11-29 10:48:47 +0700

    login user

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -16,6 +16,23 @@
         <!-- /.input-group -->
     </div>
 
+    <!-- Login Form -->
+    <div class="well">
+        <h4>Login</h4>
+        <form action="includes/login.php" method="post">
+            <div class="form-group">
+                <input name="username" type="text" class="form-control" placeholder="Enter Username">
+            </div>
+            <div class="input-group">
+                <input name="password" type="password" class="form-control" placeholder="Enter Password">
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" name="login" type="submit">Login</button>
+                </span>
+            </div>
+        </form> <!-- Form Search -->
+        <!-- /.input-group -->
+    </div>
+
     <!-- Blog Categories Well -->
     <div class="well">
         <?php 
