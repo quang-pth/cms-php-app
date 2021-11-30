db0b2b1475a669282bae280db4111aaca9253dc0
QuangPham <quang20201@gmail.com>
2021-11-30 17:27:58 +0700

    add post view count

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -43,7 +43,6 @@
                     echo '<ERROR...>';
                     break;
             }
-
         }
     }
 
@@ -74,8 +73,9 @@
             <th>Status</th>
             <th>Image</th>
             <th>Tags</th>
-            <th>Comments</th>
+            <th>Comments Count</th>
             <th>Date</th>
+            <th>Views Count</th>
             <th>View Post</th>
             <th>Edit Post</th>
             <th>Delete Post</th>
@@ -96,6 +96,7 @@
                 $post_tags = $row['post_tags'];
                 $post_comment_count = $row['post_comment_count'];
                 $post_date = $row['post_date'];
+                $post_views_count = $row['post_views_count'];
                 
                 echo "<tr>";
                 ?>
@@ -118,6 +119,7 @@
                 echo "<td>$post_tags</td>";
                 echo "<td>$post_comment_count</td>";
                 echo "<td>$post_date</td>";
+                echo "<td><a href='posts.php?reset={$post_id}'>$post_views_count</a></td>";
                 echo "<td><a href='../post.php?p_id={$post_id}'>View</a></td>";
                 echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                 echo "<td><a href='posts.php?delete={$post_id}' onclick='return confirm('Are you sure you want to delete');'>Delete</a></td>";
@@ -130,6 +132,16 @@
                 $query = "DELETE FROM posts WHERE post_id = {$post_id_to_delete} ";
                 $delete_query = mysqli_query($connection, $query);
                 confirmQuery($delete_query);
+                header("Location: posts.php");
+            }
+            
+            if(isset($_GET['reset'])) {
+                $post_id_to_reset = $_GET['reset'];
+                $query = "UPDATE posts SET post_views_count = 0 WHERE post_id = ";
+                $query .= mysqli_escape_string($connection, $post_id_to_reset);
+                $reset_query = mysqli_query($connection, $query);
+                confirmQuery($reset_query);
+                header("Location: posts.php");
             }
         ?>
     </tbody>
--- admin/js/script.js
+++ admin/js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
 
   const div_box = "<div id='load-screen'><div id='loading'></div></div>";
   $("body").prepend(div_box);
-  $('#load-screen').delay(700).fadeOut(600, function () {
+  $('#load-screen').delay(300).fadeOut(300, function () {
     $(this).remove();
   }) 
 });
